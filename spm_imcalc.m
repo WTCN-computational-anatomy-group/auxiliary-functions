77f9d9d336f968abefded34eeffae7f7f4600375
Mikael Brudfors <brudfors@gmail.com>
2018-02-28 10:00:06 +0000

    Added function for calculating image gradient.

--- spm_imcalc.m
+++ spm_imcalc.m
@@ -2,7 +2,7 @@ function varargout = spm_imcalc(varargin)
 %__________________________________________________________________________
 % Collection of tools for image calculation (gradient, suff stat, ...).
 %
-% FORMAT out = spm_imcalc(('name'), input)
+% FORMAT [Dx,Dy,Dz] = grad(X,vx)
 %
 % FORMAT help spm_imcalc>function
 % Returns the help file of the selected function.
@@ -16,10 +16,48 @@ function varargout = spm_imcalc(varargin)
     id = varargin{1};
     varargin = varargin(2:end);
     switch lower(id)
-        case 'foo'
-            [varargout{1:nargout}] = foo(varargin{:});
+        case 'grad'
+            [varargout{1:nargout}] = grad(varargin{:});
         otherwise
             help spm_imcalc
             error('Unknown function %s. Type ''help spm_imcalc'' for help.', id)
     end
 end
+%==========================================================================
+
+%==========================================================================
+function [Dx,Dy,Dz] = grad(X,vx) 
+% Calculate 2D or 3D gradient of an image
+% FORMAT [Dx,Dy,Dz] = grad(X,vx)
+% X          - Image
+% vx         - voxel size
+% [Dx,Dy,Dz] - Gradients in x-,y- and z-direction
+%__________________________________________________________________________
+% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
+   
+    if nargin<2, vx = ones(3,1); end
+
+    precision = get_type(X);
+
+    if size(X,3)==1
+        Dx = [diff(X,1,2),zeros(size(X,1),1,precision)]./vx(2);
+        Dy = [diff(X,1,1);zeros(1,size(X,2),precision)]./vx(1);
+        Dz = 0;
+    else
+        Dx = cat(2,diff(X,1,2),zeros(size(X,1),1,size(X,3),precision))./vx(2);
+        Dy = cat(1,diff(X,1,1),zeros(1,size(X,2),size(X,3),precision))./vx(1);
+        Dz = cat(3,diff(X,1,3),zeros(size(X,1),size(X,2),1,precision))./vx(3);  
+    end
+end
+%==========================================================================
+
+%==========================================================================
+% HELPER FUNCTIONS
+%==========================================================================
+
+%==========================================================================
+function out = get_type(var)
+    tmp = whos('var');
+    out = tmp.class;
+end
+%==========================================================================
