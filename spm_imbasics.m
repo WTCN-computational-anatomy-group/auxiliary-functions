fd13de23d6b9e2cf3505804bd0df6c4e826232d4
Mikael Brudfors <brudfors@gmail.com>
2018-04-18 13:40:54 +0100

    Added function to fit VB-GMM to a histogram.

--- spm_imbasics.m
+++ spm_imbasics.m
@@ -5,7 +5,8 @@ function varargout = spm_imbasics(varargin)
 % FORMAT div = dive(Dx,Dy,Dz,vx)
 % FORMAT [Dx,Dy,Dz] = grad(X,vx)
 % FORMAT smooth_img_in_mem(img,fwhm) 
-% FORMAT [mg,mn,vr] = fit_gmm2hist(h,x,K,verbose)
+% FORMAT [mg,mn,vr] = fit_gmm2hist(c,x,K,verbose)
+% FORMAT [a,m,b,n,W,mg,lb] = fit_vbgmm2hist(c,x,K,verbose)
 %
 % FORMAT help spm_imbasics>function
 % Returns the help file of the selected function.
@@ -26,6 +27,8 @@ switch lower(id)
         [varargout{1:nargout}] = smooth_img_in_mem(varargin{:});          
     case 'fit_gmm2hist'
         [varargout{1:nargout}] = fit_gmm2hist(varargin{:});                 
+    case 'fit_vbgmm2hist'
+        [varargout{1:nargout}] = fit_vbgmm2hist(varargin{:});                 
     otherwise
         help spm_imcalc
         error('Unknown function %s. Type ''help spm_imcalc'' for help.', id)
@@ -46,7 +49,7 @@ if nargin<4, vx = ones(3,1); end
 if size(Dx,3) == 1
     Du = [-Dx(:,1), -diff(Dx(:,1:end-1),1,2), Dx(:,end-1)];
     Dv = [-Dy(1,:); -diff(Dy(1:end-1,:),1,1); Dy(end-1,:)];
-    div = Du./vx(2) + Dv./vx(1);
+    div = Du./vx(2) + Dv./vx(1)% FORMAT [mg,mn,vr] = fit_gmm2hist(c,x,K,verbose);
 else
     Du = cat(2, -Dx(:,1,:), -diff(Dx(:,1:end-1,:),1,2), Dx(:,end-1,:)); 
     Dv = cat(1, -Dy(1,:,:), -diff(Dy(1:end-1,:,:),1,1), Dy(end-1,:,:));
@@ -104,10 +107,10 @@ spm_conv_vol(img,img,x,y,z,-[i j k]);
 %==========================================================================
 
 %==========================================================================
-function [mg,mn,vr] = fit_gmm2hist(h,x,K,verbose)
+function [mg,mn,vr] = fit_gmm2hist(c,x,K,verbose)
 % Fit a GMM to image histogram
-% FORMAT [mg,mn,vr] = fit_gmm2hist(h,x,K,verbose)
-% h - Histogram counts
+% FORMAT [mg,mn,vr] = fit_gmm2hist(c,x,K,verbose)
+% c - Histogram counts
 % x - Intensity values
 % K - Clusters
 % verbose - Output level [0]
@@ -138,12 +141,12 @@ for iter=1:10000
     % Sum Rule
     % p(intensity | mg, nu, sig) = \sum_k p(class=k, intensity | mg, nu, sig)
     sp         = sum(p,2)+eps;
-    ll(iter+1) = sum(log(sp).*h(:));
-    if ll(iter+1) - ll(iter) < 1e-8*sum(h)
+    ll(iter+1) = sum(log(sp).*c(:));
+    if ll(iter+1) - ll(iter) < 1e-8*sum(c)
         if verbose==2
             figure(4001);
             md = mean(diff(x));
-            plot(x(:),(h/sum(h))/md,'b-',x(:),sp,'r-'); hold on
+            plot(x(:),(c/sum(c))/md,'b-',x(:),sp,'r-'); hold on
             plot(x(:),p,'--');        
             set(gca,'TickLabelInterpreter','latex');  
             xlabel('Image intensity','Interpreter','latex')
@@ -158,7 +161,7 @@ for iter=1:10000
         figure(4001);
         subplot(121); plot(0:numel(ll)-2,ll(2:end))  
         md = mean(diff(x));
-        subplot(122); plot(x(:),p,'--',x(:),h/sum(h)/md,'b.',x(:),sp,'r'); 
+        subplot(122); plot(x(:),p,'--',x(:),c/sum(c)/md,'b.',x(:),sp,'r'); 
         drawnow
     end
 
@@ -168,9 +171,9 @@ for iter=1:10000
 
     % Compute moments from the histograms, weighted by the responsibilities (p).
     for k=1:K
-        m0(k) = sum(p(:,k).*h(:));             % Number of voxels in class k
-        m1(k) = sum(p(:,k).*h(:).*x(:));       % Sum of the intensities in class k
-        m2(k) = sum(p(:,k).*h(:).*x(:).*x(:)); % Sum of squares of intensities in class k
+        m0(k) = sum(p(:,k).*c(:));             % Number of voxels in class k
+        m1(k) = sum(p(:,k).*c(:).*x(:));       % Sum of the intensities in class k
+        m2(k) = sum(p(:,k).*c(:).*x(:).*x(:)); % Sum of squares of intensities in class k
     end
     mg = m0/sum(m0);
     for k=1:K
@@ -178,6 +181,112 @@ for iter=1:10000
         vr(k) = (m2(k)-m1(k)*m1(k)/m0(k)+1e-6)/(m0(k)+1e-6); % Variance
     end
     sd = sqrt(vr);
+end
+%==========================================================================
+
+%==========================================================================
+function [a,m,b,n,W,mg,lb] = fit_vbgmm2hist(c,x,K,verbose)
+% Fit a VB-GMM to image histogram
+% FORMAT [a,m,b,n,W,mg,lb] = fit_vbgmm2hist(c,x,K,verbose)
+%__________________________________________________________________________
+% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging   
+if nargin<4, verbose = true; end
+    
+% Define priors
+a0 = ones(1,K)/K;
+m0 = linspace(min(x),max(x),K)./K;
+b0 = ones(1,K);
+n0 = ones(1,K);
+W0 = 1./((ones(1,K)*(max(x) - min(x))./(K)).^2);
+
+% Define posteriors
+a = a0;
+m = m0;
+b = b0;
+n = n0;
+W = W0;
+
+I = numel(x);
+x = reshape(x,[I 1]);
+c = reshape(c,[I 1]);
+
+% Start algorithm
+niter = 10000;
+lb    = -Inf;
+for iter=1:niter
+    olb = lb;
+    
+    % Compute responsibilities
+    %----------------------------------------------------------------------
+    lnR = zeros(I,K);    
+    for k=1:K
+       Elnpi  = psi(a(k)) - psi(sum(a));
+       ElnLam = psi(0.5*n(k)) + log(2) + log(W(k));
+       ElnPx  = 0.5*ElnLam - 0.5*log(2*pi) - 0.5*(1/b(k) + n(k)*W(k)*(x - m(k)).*(x - m(k)));                   
+              
+       lnR(:,k) = Elnpi + ElnPx;
+    end
+    
+    % Log-sum-exp to get responsibilities
+    lnsmR = spm_matcomp('logsumexp',lnR,2);
+    lnR   = bsxfun(@minus,lnR,lnsmR);
+    R     = exp(lnR); 
+    
+    % Compute moments
+    %----------------------------------------------------------------------
+    s0 = zeros(1,K);
+    s1 = zeros(1,K);
+    S2 = zeros(1,K);
+    for k=1:K
+       s0(k) = sum(R(:,k).*c);
+       s1(k) = 1/s0(k)*sum(R(:,k).*c.*x);
+       S2(k) = 1/s0(k)*sum(R(:,k).*c.*(x - s1(k)).*(x - s1(k)));
+    end
+    
+    % Update posteriors
+    %----------------------------------------------------------------------
+    for k=1:K
+       a(k) = a0(k) + s0(k); 
+       b(k) = b0(k) + s0(k);
+       n(k) = n0(k) + s0(k);
+       m(k) = 1/b(k)*(b0(k)*m0(k) + s0(k)*s1(k));
+       
+       invW = 1/W0(k) + s0(k)*S2(k) + (b0(k)*s0(k))/(b0(k) + s0(k))*(s1(k) - m0(k))*(s1(k) - m0(k));
+       W(k) = 1/invW;
+    end    
+    
+    % Compute lower bound
+    %----------------------------------------------------------------------
+    lnpi  = bsxfun(@minus,psi(a),psi(sum(a)));
+    lnLam = psi(n/2) + log(2) + log(W);
+    
+    lnB = @(n,W) -0.5*n.*log(W) - (0.5*log(2)*n + gammaln(0.5*n));
+    lnC = @(a) gammaln(sum(a)) - (sum(gammaln(a)));
+    H   = -lnB(n,W) - 0.5*n.*lnLam + 0.5*n;
+    
+    ElnPX     = 0.5*sum(s0.*(lnLam - 1./b - n.*S2.*W - n.*W.*(s1 - m).^2 - log(2*pi)));
+    ElnPz     = sum(sum(bsxfun(@times,bsxfun(@times,R,lnpi),c)));
+    ElnPpi    = lnC(a0) + sum((a0 - 1).*lnpi);
+    ElnPmuLam = 0.5*sum(log(0.5*b0./pi) + lnLam - b0./b - b0.*n.*W.*(m - m0).^2) ...
+                + sum(lnB(n0,W0)) + sum(0.5*n0.*lnLam) - 0.5*sum(n.*(1./W0).*W);
+    ElnQZ     = sum(sum(bsxfun(@times,R.*lnR,c)));
+    ElnQpi    = sum((a - 1).*lnpi) + lnC(a);
+    ElnQmuLam = sum(0.5*lnLam + 0.5*log(b./(2*pi)) - 0.5 - H);
+    
+    lb = ElnPX + ElnPz + ElnPpi + ElnPmuLam - ElnQZ - ElnQpi - ElnQmuLam;    
+    
+    d = abs((olb*(1 + 10*eps) - lb)/lb);   
+    if verbose
+        fprintf('%i\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\n',iter,lb,d,ElnPX,ElnPz,ElnPpi,ElnPmuLam,ElnQZ,ElnQpi,ElnQmuLam);        
+    end
+        
+    if d<1e-6
+        % Expected mixing coefficients
+        mg = exp(lnpi);
+        
+        % Finished
+        break
+    end
 end
 %==========================================================================
 
