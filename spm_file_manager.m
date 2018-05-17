6338189849042348c34ab0b22bff9861636bc73e
Mikael Brudfors <brudfors@gmail.com>
2018-05-17 19:21:24 +0100

    Changed from struct to cell array, added displaying files read counter

--- spm_file_manager.m
+++ spm_file_manager.m
@@ -37,13 +37,13 @@ function dat = init_dat(dir_population,dat)
 % dir_population - Path to a directory containing JSON files. Each JSON
 % file holds subject-specific meta data.
 % S - Number of subjects to read. [S=Inf]
-% dat - A struct that contains subject-specific data. Give as input to append 
-% already exisiting dat structure with new subjects or new data for already 
-% existing subjects. [dat=struct]
+% dat - A cell-array that contains subject-specific data. Give as input to append 
+% already exisiting dat object with new subjects or new data for already 
+% existing subjects. [dat={}]
 %__________________________________________________________________________
 % Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
 if nargin<2, 
-    dat = struct; 
+    dat = {}; 
     J1  = 0; % Path to JSON file.
 else
     J1  = numel(dat);
@@ -56,10 +56,23 @@ dict = containers.Map;
 json_files = dir(fullfile(dir_population,'*.json'));
 J          = numel(json_files);
 
-for s=1:J % Loop over JSON files
+% For displaying number of files read
+base10 = floor(log10(J)) + 1;
+s      = sprintf(['spm_file_manager(''init_dat'') | %' num2str(base10) 'd of %' num2str(base10) 'd files read'],0,J);
+fprintf(1, ['%-' num2str(2*base10 + 50) 's'], s);
+    
+tic;
+for j=1:J % Loop over JSON files
+    
+    if ~mod(j,10)
+        % Display number of files read
+        fprintf(1, repmat('\b',1,2*base10 + 50));
+        s = sprintf(['spm_file_manager(''init_dat'') | %' num2str(base10) 'd of %' num2str(base10) 'd files read'],j,J);
+        fprintf(1, ['%-' num2str(2*base10 + 50) 's'], s);
+    end
     
     % Read subject-specific meta data
-    pth_json = fullfile(dir_population,json_files(s).name);
+    pth_json = fullfile(dir_population,json_files(j).name);
     metadata = spm_jsonread(pth_json);       
     
     % Check and init meta_data
@@ -75,8 +88,8 @@ for s=1:J % Loop over JSON files
     if ~dict.isKey(key)
         % Subject not in dictionary -> add subject to dictionary
         dict(key)                 = J1 + dict.Count + 1;          
-        dat(dict(key)).name       = name;
-        dat(dict(key)).population = population;
+        dat{dict(key)}.name       = name;
+        dat{dict(key)}.population = population;
     end                    
     
     if ~isempty(metadata.modality)
@@ -86,40 +99,40 @@ for s=1:J % Loop over JSON files
         Nii         = nifti(metadata.pth);
         channel     = metadata.channel;
 
-        if ~isfield(dat(dict(key)),'modality') || isempty(dat(dict(key)).modality)
+        if ~isfield(dat{dict(key)},'modality') || isempty(dat{dict(key)}.modality)
             % No image data exists -> create image data fields
-            dat(dict(key)).modality.name = modality;
+            dat{dict(key)}.modality{1}.name = modality;
             if isempty(channel)
                 % Single-channel data
-                dat(dict(key)).modality.nii      = Nii;
-                dat(dict(key)).modality.json.pth = pth_json;
+                dat{dict(key)}.modality{1}.nii      = Nii;
+                dat{dict(key)}.modality{1}.json.pth = pth_json;
             else
                 % Multi-channel data
-                dat(dict(key)).modality.channel.name      = channel;
-                dat(dict(key)).modality.channel.nii       = Nii;
-                dat(dict(key)).modality.channel.json.pth  = pth_json;
+                dat{dict(key)}.modality{1}.channel{1}.name      = channel;
+                dat{dict(key)}.modality{1}.channel{1}.nii       = Nii;
+                dat{dict(key)}.modality{1}.channel{1}.json.pth  = pth_json;
             end
         else
             % Modality field already exists for subject -> append to appropriate
             % modality and channel
-            M       = numel(dat(dict(key)).modality); % Number of modalities
+            M       = numel(dat{dict(key)}.modality); % Number of modalities
             has_mod = false;
             for m=1:M
-                if strcmp(modality,dat(dict(key)).modality(m).name)
+                if strcmp(modality,dat{dict(key)}.modality{m}.name)
                     % Modality found -> append                               
                     if isempty(channel)
-                        N = numel(dat(dict(key)).modality(m).nii);
-                        dat(dict(key)).modality(m).nii(N + 1)      = Nii;
-                        dat(dict(key)).modality(m).json(N + 1).pth = pth_json;
+                        N = numel(dat{dict(key)}.modality{m}.nii);
+                        dat{dict(key)}.modality{m}.nii(N + 1)      = Nii;
+                        dat{dict(key)}.modality{m}.json(N + 1).pth = pth_json;
                     else
-                        C       = numel(dat(dict(key)).modality(m).channel);
+                        C       = numel(dat{dict(key)}.modality{m}.channel);
                         has_chn = false;
                         for c=1:C % Loop over channels
-                            if strcmp(channel,dat(dict(key)).modality(m).channel(c).name)
+                            if strcmp(channel,dat{dict(key)}.modality{m}.channel{c}.name)
                                 % Channel found -> append
-                                N = numel(dat(dict(key)).modality(m).channel(c).nii);
-                                dat(dict(key)).modality(m).channel(c).nii(N + 1)      = Nii;
-                                dat(dict(key)).modality(m).channel(c).json(N + 1).pth = pth_json;
+                                N = numel(dat{dict(key)}.modality{m}.channel{c}.nii);
+                                dat{dict(key)}.modality{m}.channel{c}.nii(N + 1)      = Nii;
+                                dat{dict(key)}.modality{m}.channel{c}.json(N + 1).pth = pth_json;
                                 
                                 has_chn = true;
                                 break
@@ -128,9 +141,9 @@ for s=1:J % Loop over JSON files
 
                         if ~has_chn
                             % Channel not found -> add channel
-                            dat(dict(key)).modality(m).channel(C + 1).name      = channel;
-                            dat(dict(key)).modality(m).channel(C + 1).nii       = Nii;
-                            dat(dict(key)).modality(m).channel(C + 1).json.pth  = pth_json;
+                            dat{dict(key)}.modality{m}.channel{C + 1}.name      = channel;
+                            dat{dict(key)}.modality{m}.channel{C + 1}.nii       = Nii;
+                            dat{dict(key)}.modality{m}.channel{C + 1}.json.pth  = pth_json;
                         end
                     end
 
@@ -143,13 +156,13 @@ for s=1:J % Loop over JSON files
                 % Modality not found -> add modality
                 if isempty(channel)
                     % Single-channel data
-                    dat(dict(key)).modality(M + 1).nii      = Nii;
-                    dat(dict(key)).modality(M + 1).json.pth = pth_json;
+                    dat{dict(key)}.modality{M + 1}.nii      = Nii;
+                    dat{dict(key)}.modality{M + 1}.json.pth = pth_json;
                 else
                     % Multi-channel data
-                    dat(dict(key)).modality(M + 1).channel.name      = channel;
-                    dat(dict(key)).modality(M + 1).channel.nii       = Nii;
-                    dat(dict(key)).modality(M + 1).channel.json.pth  = pth_json;
+                    dat{dict(key)}.modality{M + 1}.channel{1}.name      = channel;
+                    dat{dict(key)}.modality{M + 1}.channel{1}.nii       = Nii;
+                    dat{dict(key)}.modality{M + 1}.channel{1}.json.pth  = pth_json;
                 end
             end
         end
@@ -169,14 +182,20 @@ for s=1:J % Loop over JSON files
             continue
         end
         
-        dat(dict(key)).(fn{i}) = metadata.(field_name);
+        dat{dict(key)}.(fn{i}) = metadata.(field_name);
     end            
     
     % Make sure fields are ordered alphabetically
-    dat(dict(key)) = orderfields(dat(dict(key)));
+    dat{dict(key)} = orderfields(dat{dict(key)});
 end
 
-fprintf('Loaded %i subjects into dat from %s.\n',dict.Count,dir_population);
+% Display number of files read
+fprintf(1, repmat('\b',1,2*base10 + 50));
+s = sprintf(['spm_file_manager(''init_dat'') | %' num2str(base10) 'd of %' num2str(base10) 'd files read'],j,J);
+fprintf(1, ['%-' num2str(2*base10 + 50) 's'], s);
+fprintf('\n');
+
+fprintf('Loaded %i subjects from %s in %0.1f seconds.\n',dict.Count,dir_population,toc);
 %==========================================================================
 
 %==========================================================================
