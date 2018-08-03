cabf5c6416853ce473d9841bfee79a8f4db196f7
Mikael Brudfors <brudfors@gmail.com>
2018-08-03 11:43:51 +0100

    Removed code that is no longer used, or necessary

--- spm_misc.m
+++ spm_misc.m
@@ -9,9 +9,6 @@ function varargout = spm_misc(varargin)
 % FORMAT nw = spm_misc('nbr_parfor_workers')
 % FORMAT vx = spm_misc('vxsize',M)
 % FORMAT msk = spm_misc('msk_modality',f,modality)
-% FORMAT spm_misc('browse_subjects',pth,modality)
-% FORMAT mom = spm_misc('mom_John2Bishop',mom)
-% FORMAT mom = spm_misc('mom_Bishop2John',mom)
 % FORMAT gain = spm_misc('get_gain',vals)
 %
 % FORMAT help spm_parfor>function
@@ -38,13 +35,7 @@ switch lower(id)
     case 'vxsize'
         [varargout{1:nargout}] = vxsize(varargin{:});              
     case 'msk_modality'
-        [varargout{1:nargout}] = msk_modality(varargin{:});          
-    case 'browse_subjects'
-        [varargout{1:nargout}] = browse_subjects(varargin{:});     
-    case 'mom_john2bishop'
-        [varargout{1:nargout}] = mom_John2Bishop(varargin{:});           
-    case 'mom_bishop2john'
-        [varargout{1:nargout}] = mom_Bishop2John(varargin{:});              
+        [varargout{1:nargout}] = msk_modality(varargin{:});                
     case 'get_gain'
         [varargout{1:nargout}] = get_gain(varargin{:});          
     otherwise
@@ -277,75 +268,6 @@ if strcmpi(modality,'mri'),
     msk = isfinite(f) & (f~=0);
 elseif strcmp(modality,'CT'), 
     msk = isfinite(f) & (f~=max(f(:))) & (f~=min(f(:))) & (f~=0);
-end
-%==========================================================================
-
-%==========================================================================
-function browse_subjects(pth,modality)
-% Displays a populations of images
-% FORMAT browse_subjects(pth,modality)
-%__________________________________________________________________________
-% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
-folder    = dir(pth);
-folder    = folder(3:end);
-dirflag   = [folder.isdir];
-subfolder = folder(dirflag);
-S         = numel(subfolder);
-
-fname = {};
-cnt   = 1;
-for s=1:S
-    dir_scans = fullfile(pth,subfolder(s).name,'scans',modality);        
-
-    scans = dir(fullfile(dir_scans,'*.nii'));
-    if isempty(scans)
-        scans = dir(fullfile(dir_scans,'*.img'));
-    end
-
-    if ~isempty(scans)
-        fname{cnt} = fullfile(dir_scans,scans(1).name);       
-        cnt        = cnt + 1;
-    end
-end 
-
-spm_check_registration(char(fname));
-%==========================================================================
-
-%==========================================================================
-function mom = mom_John2Bishop(mom)
-% Converts JA's representation of moments to Bishops'
-% FORMAT mom = mom_John2Bishop(mom)
-%__________________________________________________________________________
-% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
-K = numel(mom(1).s0);
-for i=1:numel(mom) 
-    s1 = zeros(size(mom(i).s1));
-    S2 = zeros(size(mom(i).S2));
-    for k=1:K
-        s1(:,k)   = mom(i).s1(:,k)/mom(i).s0(k);
-        S2(:,:,k) = mom(i).S2(:,:,k)/mom(i).s0(k) - (mom(i).s1(:,k)/mom(i).s0(k))*(mom(i).s1(:,k)/mom(i).s0(k))';
-    end
-    mom(i).s1 = s1;
-    mom(i).S2 = S2;
-end
-%==========================================================================
-
-%==========================================================================
-function mom = mom_Bishop2John(mom)
-% Converts Bishops' representation of moments to JA's
-% FORMAT mom = mom_Bishop2John(mom)
-%__________________________________________________________________________
-% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
-K = numel(mom(1).s0);
-for i=1:numel(mom) 
-    s1 = zeros(size(mom(i).s1));
-    S2 = zeros(size(mom(i).S2));
-    for k=1:K
-        s1(:,k)   = mom(i).s0(k)*mom(i).s1(:,k);
-        S2(:,:,k) = mom(i).s0(k)*mom(i).S2(:,:,k) + mom(i).s0(k)*(s1(:,k) /mom(i).s0(k))*(s1(:,k) /mom(i).s0(k))';
-    end
-    mom(i).s1 = s1;
-    mom(i).S2 = S2;
 end
 %==========================================================================
 
