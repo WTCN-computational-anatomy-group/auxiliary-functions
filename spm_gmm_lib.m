13c91c10a006785d9f0f69b062f35589d7f25bbd
Mikael Brudfors <brudfors@gmail.com>
2018-08-06 17:04:57 +0100

    Added code to replace a single VB Gaussian by a bunch of VB Gaussians
    FORMAT gmm = spm_gmm_lib('extras', 'more_gmms', gmm, part)

--- spm_gmm_lib.m
+++ spm_gmm_lib.m
@@ -124,6 +124,13 @@ function varargout = spm_gmm_lib(varargin)
 % c = spm_gmm_lib('plot', 'cat2rgb', f, pal)
 % > Generate an RGB volume from a categorical (e.g. responsibilities) volume.
 %
+%--------------------------------------------------------------------------
+% Extras
+% -------------
+%
+% gmm = spm_gmm_lib('extras', 'more_gmms', gmm, part)
+% > A crude heuristic to replace a single Gaussian by a bunch of Gaussians.
+%
 %__________________________________________________________________________
 % Copyright (C) 2018 Wellcome Centre for Human Neuroimaging
 
@@ -164,6 +171,8 @@ switch lower(id)
         [varargout{1:nargout}] = double2int(varargin{:});  
     case 'plot'
         [varargout{1:nargout}] = gmmplot(varargin{:});       
+    case 'extras'
+        [varargout{1:nargout}] = gmm_extras(varargin{:});             
     otherwise
         help spm_gmm_lib
         error('Unknown function %s. Type ''help spm_gmm_lib'' for help.', id)
@@ -2058,6 +2067,84 @@ end
 if tri
     c = reshape(c, [size(c, 1) size(c, 2) 1 size(c, 3)]);
 end
+%==========================================================================
+
+% =========================================================================
+function varargout = gmm_extras(varargin)
+% Additional GMM functions
+%
+% gmm = spm_gmm_lib('extras', 'more_gmms', gmm, part)
+% > A crude heuristic to replace a single VB Gaussian by a bunch of VB Gaussians.
+%
+
+if nargin == 0
+    help spm_gmm_lib>extras
+    error('Not enough argument. Type ''help spm_gmm_lib>plot'' for help.');
+end
+id = varargin{1};
+varargin = varargin(2:end);
+switch lower(id)
+    case {'more_gmms'}
+        [varargout{1:nargout}] = more_gmms(varargin{:});      
+    otherwise
+        help spm_gmm_lib>extras
+        error('Unknown function %s. Type ''help spm_gmm_lib>extras'' for help.', id)
+end
+% =========================================================================
+
+% =========================================================================
+function gmm = more_gmms(gmm,part)
+% FORMAT gmm = spm_gmm_lib('extras', 'more_gmms', gmm, part)
+%
+% gmm  - Cell with the following format {m,b,W,n}, where there are K
+%        Gaussians.
+% part - [1,K_p] vector partitioning a K GMM into a K_p GMM (K_P>=K). E.g.
+%        [1 1 1 2 3 4 5 6 6] means that the first Gaussian will be divided
+%        into 3 and the last into 2. The rest will remain the same.
+%
+% gmm  - Cell with the following format {m,b,W,n}, where there are K_p 
+%        Gaussians.
+%
+% A crude heuristic to replace a single VB Gaussian by a bunch of VB Gaussians.
+% If there is only one Gaussian, then it should be the same as the
+% original distribution.
+MU0 = gmm{1};
+b0  = gmm{2};
+W0  = gmm{3};
+n0  = gmm{4};
+
+K   = numel(n0);
+K_p = numel(part);
+C   = size(MU0,1);
+
+A0 = bsxfun(@times,W0,reshape(n0,[1 1 K])); % E[Lambda]
+
+m = zeros(C,K_p);
+b = zeros(1,K_p);
+W = zeros(C,C,K_p);
+n = zeros(1,K_p); % A = nW, W = 1/n*inv(Cov)
+
+for k=1:K    
+    kk = sum(part==k);
+    w  = 1./(1 + exp(-(kk - 1)*0.25)) - 0.5;
+    mn = MU0(:,k);
+    vr = inv(A0(:,:,k));
+    
+    mn = sqrtm(vr)*randn(C,kk)*w + repmat(mn,[1 kk]);
+    vr = vr*(1 - w);
+    pr = inv(vr);
+    W1 = (1/n0(k))*pr;
+    
+    m(:,part==k)   = mn;
+    b(part==k)     = b0(k);
+    W(:,:,part==k) = repmat(W1,[1 1 kk]);
+    n(part==k)     = n0(k);
+end
+
+gmm{1} = m;
+gmm{2} = b;
+gmm{3} = W;
+gmm{4} = n;
 %==========================================================================
 
 %==========================================================================
