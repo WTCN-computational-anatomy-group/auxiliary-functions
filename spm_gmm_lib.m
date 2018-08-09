06b5a03c6264775ef46992731e69e5b6c73a8410
Mikael Brudfors <brudfors@gmail.com>
2018-08-09 19:24:23 +0100

    Extra terms can be added to the responsibilities by varargin

--- spm_gmm_lib.m
+++ spm_gmm_lib.m
@@ -401,7 +401,7 @@ end
 logpX(all(isnan(X),2),:) = NaN;
 
 % =========================================================================
-function Z = responsibility(logpX, logPI)
+function Z = responsibility(logpX, logPI, varargin)
 % FORMAT Z = spm_gmm_lib('responsibility', logpX, logPI)
 %
 % Compute responsibilities.
@@ -409,12 +409,20 @@ function Z = responsibility(logpX, logPI)
 % vectors z_n.
 % The posterior is computed as:
 %   r_nk = exp(E[log Pi_k] + E[log p(x_n | Theta_k)]) / sum_k {r_nk}
+%
+% Extra terms can be added to the responsibilities prior to softmax by the
+% varargin argument. These arguments need to be compatible (w.r.t. size) with 
+% the following function call: bsxfun(@plus, Z, varargin{i}).
 
 % Omit NaN
 logpX(isnan(logpX)) = 0; 
 
 % Add terms: E[log Pi_k] + E[log p(X | Theta_k)]
 Z = bsxfun(@plus, logpX, logPI);
+
+for i=1:numel(varargin)
+    Z = bsxfun(@plus, Z, varargin{i});
+end
 
 % Exponentiate and normalise
 Z = bsxfun(@minus, Z, max(Z, [], 2));
