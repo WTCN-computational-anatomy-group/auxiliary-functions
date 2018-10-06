ac8c42f4c157535b509a2ac5fe04ad878c653bf3
Mikael Brudfors <brudfors@gmail.com>
2018-10-06 18:02:21 +0100

    Fixes and improvements to grad and dive

--- spm_imbasics.m
+++ spm_imbasics.m
@@ -40,48 +40,46 @@ end
 
 %==========================================================================
 function div = dive(Dx,Dy,Dz,vx)  
-% Computes the divergence of an image
+% Computes the divergence of an image (with voxel size)
 % FORMAT div = dive(Dx,Dy,Dz,vx) 
 % [Dx,Dy,Dz] - Gradients in x-,y- and z-direction
 % vx         - Voxel size
 % div        - Divergence
 %__________________________________________________________________________
 % Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
-if nargin<4, vx = ones(3,1); end
+if nargin < 4, vx = ones([1 3],'like',Dx); end
 
 if size(Dx,3) == 1
-    Du = [-Dx(:,1), -diff(Dx(:,1:end-1),1,2), Dx(:,end-1)];
-    Dv = [-Dy(1,:); -diff(Dy(1:end-1,:),1,1); Dy(end-1,:)];
-    div = Du./vx(2) + Dv./vx(1);
+    Du  = [-Dx(:,1), -diff(Dx(:,1:end-1),1,2), Dx(:,end-1)];
+    Dv  = [-Dy(1,:); -diff(Dy(1:end-1,:),1,1); Dy(end-1,:)];
+    div = Du./vx(1) + Dv./vx(2);
 else
-    Du = cat(2, -Dx(:,1,:), -diff(Dx(:,1:end-1,:),1,2), Dx(:,end-1,:)); 
-    Dv = cat(1, -Dy(1,:,:), -diff(Dy(1:end-1,:,:),1,1), Dy(end-1,:,:));
-    Dw = cat(3, -Dz(:,:,1), -diff(Dz(:,:,1:end-1),1,3), Dz(:,:,end-1));
-    div = Du./vx(2) + Dv./vx(1) + Dw./vx(3);
+    Du  = cat(2, -Dx(:,1,:), -diff(Dx(:,1:end-1,:),1,2), Dx(:,end-1,:)); 
+    Dv  = cat(1, -Dy(1,:,:), -diff(Dy(1:end-1,:,:),1,1), Dy(end-1,:,:));
+    Dw  = cat(3, -Dz(:,:,1), -diff(Dz(:,:,1:end-1),1,3), Dz(:,:,end-1));
+    div = Du./vx(1) + Dv./vx(2) + Dw./vx(3);
 end
 %==========================================================================
 
 %==========================================================================
 function [Dx,Dy,Dz] = grad(X,vx) 
-% Calculate 2D or 3D gradient of an image
+% Calculate 2D or 3D gradient of an image (with voxel size)
 % FORMAT [Dx,Dy,Dz] = grad(X,vx)
 % X          - Image
 % vx         - voxel size
 % [Dx,Dy,Dz] - Gradients in x-,y- and z-direction
 %__________________________________________________________________________
 % Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging   
-if nargin<2, vx = ones(3,1); end
-
-precision = get_type(X);
+if nargin < 2, vx = ones([1 3],'like',X); end
 
 if size(X,3)==1
-    Dx = [diff(X,1,2),zeros(size(X,1),1,precision)]./vx(2);
-    Dy = [diff(X,1,1);zeros(1,size(X,2),precision)]./vx(1);
+    Dx = [diff(X,1,2),zeros(size(X,1),1,'like',X)]./vx(2);
+    Dy = [diff(X,1,1);zeros(1,size(X,2),'like',X)]./vx(1);
     Dz = 0;
 else
-    Dx = cat(2,diff(X,1,2),zeros(size(X,1),1,size(X,3),precision))./vx(2);
-    Dy = cat(1,diff(X,1,1),zeros(1,size(X,2),size(X,3),precision))./vx(1);
-    Dz = cat(3,diff(X,1,3),zeros(size(X,1),size(X,2),1,precision))./vx(3);  
+    Dx = cat(2,diff(X,1,2),zeros(size(X,1),1,size(X,3),'like',X))./vx(2);
+    Dy = cat(1,diff(X,1,1),zeros(1,size(X,2),size(X,3),'like',X))./vx(1);
+    Dz = cat(3,diff(X,1,3),zeros(size(X,1),size(X,2),1,'like',X))./vx(3);  
 end
 %==========================================================================
 
